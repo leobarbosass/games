d6213b759c6c7f75760c294404ebe6d25e486796
leo <leoextreme12345678910@gmail.com>
2022-09-30 11:28:32 -0300

    add

---
+++ app/src/main/java/br/senai/sp/jandira/games/Activity2.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.games
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
